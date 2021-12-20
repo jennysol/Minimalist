e2f6f48ea7ff4e2b25d6690eec13761ceaa1f56b
jennysol <jennyssoliver@gmail.com>
2021-12-20 20:05:49 -0300

    Update
    Unity test with model admin is valid and not valid

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Admin, type: :model do
     expect(admin.email).to eql 'admin@social.com'
     expect(admin).to be_valid
   end
+
+  it "admin is not valid" do 
+    admin = Admin.new(email: 'admin@social.com')
+    
+    expect(admin).to_not be_valid
+  end
 end
