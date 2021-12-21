c36ffb891c76b366f38ab5f531c88b81e8beba58
jennysol <jennyssoliver@gmail.com>
2021-12-21 20:59:46 -0300

    Feature
    Add seed for create admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Admin
+Admin.create(email: 'admin@social.com', password: '123456')
