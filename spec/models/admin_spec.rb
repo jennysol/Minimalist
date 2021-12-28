8c4db396806cd9ef5e4521e948dcdd6d133774fc
jennysol <jennyssoliver@gmail.com>
2021-12-28 16:34:56 -0300

    Feature
    Add column name for admins

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it "admin is valid" do 
-    admin = Admin.new(email: 'admin@social.com', password: '123456')
+    admin = Admin.new(email: 'admin@social.com', password: '123456', name: 'Admin')
 
     expect(admin.email).to eql 'admin@social.com'
     expect(admin).to be_valid
