70c243ec491229813329844ba61d563f4322de36
jennysol <jennyssoliver@gmail.com>
2021-12-28 11:37:04 -0300

    Feature: Add admin controller and skip registratio

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ApplicationController
+  before_action :authenticate_admin
+
+  def authenticate_admin
+    redirect_to new_admin_session_path unless current_admin?
+  end
+
+  def current_admin? #True or false
+    if current_admin.pressent?
+      true
+    else
+      false
+    end
+  end
+end
