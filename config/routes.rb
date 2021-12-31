3431f7ee4e43ce1638c6f989d45cfb7bb538cac1
jennysol <jennyssoliver@gmail.com>
2021-12-31 15:54:56 -0300

    Feature
    Add register user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
 
   namespace :admin do 
