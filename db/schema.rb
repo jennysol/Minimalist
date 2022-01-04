2df46397d9d5ba3fe500d27d1b30bf8c12845639
jennysol <jennyssoliver@gmail.com>
2022-01-04 07:40:11 -0300

    Feature
    Add subscriptions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_195943) do
+ActiveRecord::Schema.define(version: 2022_01_04_103608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2021_12_31_195943) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "followed_id"
+    t.bigint "followed_by_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_by_id"], name: "index_subscriptions_on_followed_by_id"
+    t.index ["followed_id"], name: "index_subscriptions_on_followed_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +90,6 @@ ActiveRecord::Schema.define(version: 2021_12_31_195943) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "subscriptions", "users", column: "followed_by_id"
+  add_foreign_key "subscriptions", "users", column: "followed_id"
 end
