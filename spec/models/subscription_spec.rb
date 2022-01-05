87ead1deed808d157d32bdf5a2ca689cb6c9e809
jennysol <jennyssoliver@gmail.com>
2022-01-05 20:50:20 -0300

    Feature
    Add spec for subscriptions

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -8,5 +8,27 @@ RSpec.describe Subscription, type: :model do
 
     Subscription.create(followed: boby, followed_by: matt)
     
+    expect(boby.followers.count).to eql 1
+    expect(boby.followers).to include matt
+
+    expect(matt.following.count).to eql 1
+    expect(matt.following).to include boby
+
+    expect(boby.following.count).to eql 0
+    expect(boby.following).to_not include matt
+
+    expect(matt.followers.count).to eql 0
+    expect(matt.followers).to_not include boby
+
+    donald = FactoryBot.create(:user)
+
+    Subscription.create(followed: boby, followed_by: donald)
+
+    expect(boby.followers.count).to eql 2
+    expect(boby.followers).to include donald
+
+    expect(donald.following.count).to eql 1
+    expect(donald.following).to include boby
+    
   end
 end
