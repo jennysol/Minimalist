2e43a85f3a8b821ae268bfbd03ceb0bde421bc27
jennysol <jennyssoliver@gmail.com>
2022-01-05 07:55:03 -0300

    Fix: Post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Post, type: :model do
   it "post is valid" do 
 
     post = FactoryBot.build(:post)
-    post.user_id = FactoryBot.build(:user).id
+    post.user = FactoryBot.build(:user)
     post.save
 
     expect(post).to be_valid
@@ -12,14 +12,14 @@ RSpec.describe Post, type: :model do
 
   it "post belongs to user" do
     user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post, user_id: user.id)
+    post = FactoryBot.create(:post, user: user)
 
     expect(post.user_id).to eql user.id
   end
 
   it "user has many posts" do
     user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post, user_id: user.id)
+    post = FactoryBot.create(:post, user: user)
 
     expect(user.posts).to include post
   end
