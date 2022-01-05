5f0fcf48ad8c87c89b63667930324fa2cccdab00
jennysol <jennyssoliver@gmail.com>
2022-01-05 07:54:35 -0300

    Update: Factory user rando email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do 
   factory :user do
     name { "User" }
-    email { "user@user.com" }
+    email { "user#{rand(1..100)}@user.com" }
     phone { "26484494" }
     avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png')}
     password { "123456" }
