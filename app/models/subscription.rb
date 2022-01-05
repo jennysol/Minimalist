815476084e094af399bec56ff20f3032fdc6c652
jennysol <jennyssoliver@gmail.com>
2022-01-05 07:55:33 -0300

    Fix: Subescription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ApplicationRecord
-  belongs_to :followed, class: 'User'
-  belongs_to :followed_by, class: 'User'
+  belongs_to :followed, class_name: 'User'
+  belongs_to :followed_by, class_name: 'User'
 end
   
