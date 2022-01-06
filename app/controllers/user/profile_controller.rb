4ae813eaed19857a9822f1492ba8c017306de8ca
jennysol <jennyssoliver@gmail.com>
2022-01-05 21:55:20 -0300

    Fix
    Clean subscriptions controller

--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -5,7 +5,7 @@ class User::ProfileController < UserController
 	end
 
   def potential_to_follow
-    @potential_to_follow = User.where.not(id: current_user.following.pluck(:id)).where.not(id: current_user.id)
+    @potential_to_follow = User.potential_to_follow(current_user)
   end
 	
 end
